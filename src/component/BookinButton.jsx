bdc9e60b8d06bfa024869ca6d8cb029be0321446
surayaafroze63 <surayaafroze63@gmail.com>
2026-05-21 00:52:20 +0600

    create my bookings page

--- src/component/BookinButton.jsx
+++ src/component/BookinButton.jsx
@@ -1,23 +1,75 @@
-'use client'
+'use client';
+
 import { authClient } from '@/lib/auth-client';
 import React from 'react';
 
-const BookinButton = () => {
-   const { data: session } = authClient.useSession();
-const user = session?.user;
-  console.log(user)
+const BookinButton = ({ room }) => {
 
-// const handlebooking =async () =>{
-// const bookingData = {
-  
-// }
-// }
+  const { data: session } = authClient.useSession();
+
+  const user = session?.user;
+
+  const {
+    _id,
+    roomName,
+    description,
+    image,
+    amenities,
+    capacity,
+    floor,
+    hourlyRate
+  } = room || {};
+
+  const handlebooking = async () => {
+
+    if (!user) {
+      alert("Please login first");
+      return;
+    }
+
+    const bookingData = {
+      userId: user.id,
+      userImage: user.image,
+      userName: user.name,
+      userEmail: user.email,
+
+      bookingId: _id,
+      roomName,
+      description,
+      image,
+      amenities,
+      capacity,
+      floor,
+      hourlyRate,
+    };
+
+    console.log(bookingData);
+
+    const res = await fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(bookingData),
+    });
+
+    const data = await res.json();
+
+    console.log(data);
+
+    if (data.insertedId) {
+      alert("Booking Successful");
+    }
+  };
 
   return (
     <div>
-        <button className="w-full py-4 rounded-2xl bg-indigo-600 hover:bg-indigo-700 text-white font-semibold text-lg shadow-md transition">
-                   Book This Room
-                 </button>
+      <button
+        onClick={handlebooking}
+        className="w-full py-4 rounded-2xl bg-indigo-600 hover:bg-indigo-700 text-white font-semibold text-lg shadow-md transition"
+      >
+        Book This Room
+      </button>
     </div>
   );
 };
